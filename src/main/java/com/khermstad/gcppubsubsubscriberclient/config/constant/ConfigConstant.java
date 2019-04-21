37c9fae585d5f1f0dc4ab1049bd0f75abd8a07e6
Kris Hermstad <krishermstad@gmail.com>
2019-04-21 04:34:31 -0400

    basic payload subscription functionality

---
+++ src/main/java/com/khermstad/gcppubsubsubscriberclient/config/constant/ConfigConstant.java
@@ -0,0 +1,6 @@
+package com.khermstad.gcppubsubsubscriberclient.config.constant;
+
+public class ConfigConstant {
+    final public static String GCP_PUBSUB_SUBSCRIPTION_NAME = "${spring.cloud.gcp.pubsub.subscription.name}"; 
+    final public static String SPRING_PUBSUB_INPUT_CHANNEL = "pubsubInputChannel";
+}
---
+++ src/main/java/com/khermstad/gcppubsubsubscriberclient/config/handler/SubscriptionMessageHandler.java
@@ -0,0 +1,23 @@
+package com.khermstad.gcppubsubsubscriberclient.config.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.stereotype.Component;
+
+import static com.khermstad.gcppubsubsubscriberclient.config.constant.ConfigConstant.SPRING_PUBSUB_INPUT_CHANNEL;
+
+@Component
+@Slf4j
+public class SubscriptionMessageHandler {
+    
+    @Bean
+    @ServiceActivator(inputChannel = SPRING_PUBSUB_INPUT_CHANNEL)
+    public MessageHandler messageReceiver() {
+        return message -> {
+            log.info("Message arrived! Payload: " + new String((byte[]) message.getPayload()));
+        };
+    }
+    
+}
