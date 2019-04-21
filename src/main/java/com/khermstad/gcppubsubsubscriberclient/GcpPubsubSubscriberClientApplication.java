ab465dc240ed53bcda837f3e14290ff0a23a71db
Kris Hermstad <krishermstad@gmail.com>
2019-04-21 03:44:12 -0400

    initial commit

---
+++ src/main/java/com/khermstad/gcppubsubsubscriberclient/GcpPubsubSubscriberClientApplication.java
@@ -0,0 +1,13 @@
+package com.khermstad.gcppubsubsubscriberclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpPubsubSubscriberClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcpPubsubSubscriberClientApplication.class, args);
+    }
+
+}
